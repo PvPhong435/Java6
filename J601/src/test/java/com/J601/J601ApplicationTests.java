0c355b122f656c14c1f8bb4863978cef12095e86
Pham Van Phong <vanphongdz435@gmail.com>
2024-10-29 20:22:13 +0700

    học về biểu thức Lambda

---
+++ J601/src/test/java/com/J601/J601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.J601;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
